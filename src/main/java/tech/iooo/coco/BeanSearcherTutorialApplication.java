1fa6e8e1c23fe1c698bb164ffdb0f48d87e70dcc
龙也 <yanchi.zyc@alibaba-inc.com>
2022-10-27 10:26:21 +0800

    [REFACTOR]:[init]

---
+++ src/main/java/tech/iooo/coco/BeanSearcherTutorialApplication.java
@@ -0,0 +1,13 @@
+package tech.iooo.coco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanSearcherTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeanSearcherTutorialApplication.class, args);
+    }
+
+}
